686d895b05d7222dd976a2445dcdbe1265414c57
KingWQ <1573426145@qq.com>
2023-01-06 23:18:30 +0800

    config和env配置

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,9 @@
 package config
 
 import (
+	"github.com/spf13/cast"
 	viperlib "github.com/spf13/viper" //自定义包名，避免与内置viper
+	"os"
 )
 
 // viper库实例
@@ -32,3 +34,79 @@ func init() {
 
 	ConfigFuncs = make(map[string]ConfigFunc)
 }
+
+// InitConfig 初始化配置信息 完成对环境变量以及 config 信息的加载
+func InitConfig(env string) {
+	//1. 加载环境变量
+	loadEnv(env)
+
+	//2. 注册配置信息
+	loadConfig()
+}
+
+// 加载环境变量
+func loadConfig() {
+	for name, fn := range ConfigFuncs {
+		viper.Set(name, fn())
+	}
+}
+
+// 注册配置信息
+func loadEnv(envSuffix string) {
+	// 默认加载 .env 文件，如果有传参 --env=name的话，加载.env.name文件
+	envPath := ".env"
+	if len(envSuffix) > 0 {
+		filepath := ".env." + envSuffix
+		if _, err := os.Stat(filepath); err == nil {
+			// 如 .env.testing 或 .env.stage
+			envPath = filepath
+		}
+	}
+
+	// 加载 env
+	viper.SetConfigName(envPath)
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	// 监控 .env 文件，变更时重新加载
+	viper.WatchConfig()
+}
+
+// Env 读取环境变量，支持默认值
+func Env(envName string, defaultValue ...interface{}) interface{} {
+	if len(defaultValue) > 0 {
+		return internalGet(envName, defaultValue[0])
+	}
+	return internalGet(envName)
+}
+
+// Add 新增
+func Add(name string, configFn ConfigFunc) {
+	ConfigFuncs[name] = configFn
+}
+
+// Get 获取配置项
+// 第一个参数 path 允许使用点式获取 如：app.name
+// 第二个参数允许传参默认值
+func Get(path string, defaultValue ...interface{}) string {
+	return GetString(path, defaultValue...)
+}
+
+func internalGet(path string, defaultValue ...interface{}) interface{} {
+	// config 或者环境变量不存在的情况
+	if !viper.IsSet(path) || helpers.Empty(viper.Get(path)) {
+		if len(defaultValue) > 0 {
+			return defaultValue[0]
+		}
+		return nil
+	}
+	return viper.Get(path)
+}
+
+// GetString 获取 String 类型的配置信息
+func GetString(path string, defaultValue ...interface{}) string {
+	return cast.ToString(internalGet(path, defaultValue...))
+}
+
+// GetInt
