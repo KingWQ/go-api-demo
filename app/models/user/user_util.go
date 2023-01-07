1ac0a0a009bd6a0fd095ecc105b01f1aa1b1e3ac
KingWQ <1573426145@qq.com>
2023-01-07 23:04:42 +0800

    用户模型

---
+++ app/models/user/user_util.go
@@ -0,0 +1,17 @@
+package user
+
+import "go-api-demo/pkg/database"
+
+// IsEmailExist 判断Email是否注册
+func IsEmailExist(email string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("email = ?", email).Count(&count)
+	return count > 0
+}
+
+// IsPhoneExist 判断手机号是否注册
+func IsPhoneExist(phone string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
+	return count > 0
+}
