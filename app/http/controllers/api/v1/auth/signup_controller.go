6ed55e70471bc554e73fa025287d9dad349b5d90
KingWQ <1573426145@qq.com>
2023-01-08 22:51:06 +0800

    json解析和验证器封装

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -2,7 +2,6 @@
 package auth
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	v1 "go-api-demo/app/http/controllers/api/v1"
 	"go-api-demo/app/models/user"
@@ -17,33 +16,13 @@ type SignupController struct {
 
 // IsPhoneExist 检测手机号是否已注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
-	//1.初始化请求对象
+	//1. 获取请求参数，并做表单验证
 	request := requests.SignupPhoneExistRequest{}
-
-	//2.解析json请求
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-
-		// 打印错误信息
-		fmt.Println(err.Error())
-
-		//出错了 中断
+	if ok := requests.Validate(c, &request, requests.SignupPhoneExist); !ok {
 		return
 	}
 
-	//3.表单验证
-	errs := requests.ValidateSignupPhoneExist(&request, c)
-	if len(errs) > 0 {
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
-		return
-	}
-
-	//4.检查数据库并返回响应
+	//2. 检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
@@ -51,33 +30,13 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 
 // IsEmailExist 检测手机号是否已注册
 func (sc *SignupController) IsEmailExist(c *gin.Context) {
-	//1.初始化请求对象
+	//1. 获取请求参数，并做表单验证
 	request := requests.SignupEmailExistRequest{}
-
-	//2.解析json请求
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-
-		// 打印错误信息
-		fmt.Println(err.Error())
-
-		//出错了 中断
+	if ok := requests.Validate(c, &request, requests.SignupEmailExist); !ok {
 		return
 	}
 
-	//3.表单验证
-	errs := requests.ValidateSignupEmailExist(&request, c)
-	if len(errs) > 0 {
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
-		return
-	}
-
-	//4.检查数据库并返回响应
+	//2. 检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsEmailExist(request.Email),
 	})
--- app/requests/signup_requests.go
+++ app/requests/signup_requests.go
@@ -6,14 +6,10 @@ import (
 )
 
 type SignupPhoneExistRequest struct {
-	Phone string `json:"phone,omitempty" valid:"phone"`
+	Phone string `json:"phone,omitempty" form:"phone,omitempty" valid:"phone"`
 }
 
-type SignupEmailExistRequest struct {
-	Email string `json:"email,omitempty" valid:"email"`
-}
-
-func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
+func SignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
 	//自定义验证规则
 	rules := govalidator.MapData{
 		"phone": []string{"required", "digits:11"},
@@ -26,21 +22,14 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 			"digits:手机号长度必须为 11 位数字",
 		},
 	}
-
-	//配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", //模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	//开始验证
-	return govalidator.New(opts).ValidateStruct()
-
+	return validate(data, rules, messages)
 }
 
-func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
+type SignupEmailExistRequest struct {
+	Email string `json:"email,omitempty" form:"email,omitempty" valid:"email"`
+}
+
+func SignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
 	//自定义验证规则
 	rules := govalidator.MapData{
 		"email": []string{"required", "min:4", "max:30", "email"},
@@ -56,15 +45,5 @@ func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]str
 		},
 	}
 
-	//配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", //模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	//开始验证
-	return govalidator.New(opts).ValidateStruct()
-
+	return validate(data, rules, messages)
 }
