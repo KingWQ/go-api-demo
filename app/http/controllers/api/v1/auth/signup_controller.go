e12fef7ba1bd634597b841b160a52a5aa6cf84ac
KingWQ <1573426145@qq.com>
2023-01-08 10:48:16 +0800

    Email 是否已注册

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -15,6 +15,7 @@ type SignupController struct {
 	v1.BaseAPIController
 }
 
+// IsPhoneExist 检测手机号是否已注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	//1.初始化请求对象
 	request := requests.SignupPhoneExistRequest{}
@@ -45,5 +46,39 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	//4.检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
+	})
+}
+
+// IsEmailExist 检测手机号是否已注册
+func (sc *SignupController) IsEmailExist(c *gin.Context) {
+	//1.初始化请求对象
+	request := requests.SignupEmailExistRequest{}
+
+	//2.解析json请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// 解析失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+
+		// 打印错误信息
+		fmt.Println(err.Error())
+
+		//出错了 中断
+		return
+	}
+
+	//3.表单验证
+	errs := requests.ValidateSignupEmailExist(&request, c)
+	if len(errs) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
+
+	//4.检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsEmailExist(request.Email),
 	})
 }
