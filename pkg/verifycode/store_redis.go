0336f83345baa8067ca6e3d4a576277d6c1b4e87
KingWQ <1573426145@qq.com>
2023-01-18 23:18:02 +0800

    验证码库

---
+++ pkg/verifycode/store_redis.go
@@ -0,0 +1,39 @@
+package verifycode
+
+import (
+	"go-api-demo/pkg/app"
+	"go-api-demo/pkg/config"
+	"go-api-demo/pkg/redis"
+	"time"
+)
+
+// RedisStore 实现verifycode.Store interface
+type RedisStore struct {
+	RedisClient *redis.RedisClient
+	KeyPrefix   string
+}
+
+// Set 实现verifycode.Store interface 的 Set 方法
+func (s *RedisStore) Set(key, value string) bool {
+	ExpireTime := time.Minute * time.Duration(config.GetInt64("verifycode.expire_time"))
+	if app.IsLocal() {
+		ExpireTime = time.Minute * time.Duration(config.GetInt64("verifycode.debug_expire_time"))
+	}
+	return s.RedisClient.Set(s.KeyPrefix+key, value, ExpireTime)
+}
+
+// Get 实现verifycode.Store interface 的 Get 方法
+func (s *RedisStore) Get(key string, clear bool) (value string) {
+	key = s.KeyPrefix + key
+	val := s.RedisClient.Get(key)
+	if clear {
+		s.RedisClient.Del(key)
+	}
+	return val
+}
+
+// Verify 实现verifycode.Store interface 的 Verify 方法
+func (s *RedisStore) Verify(key, answer string, clear bool) bool {
+	v := s.Get(key, clear)
+	return v == answer
+}
---
+++ pkg/verifycode/verifycode.go
@@ -0,0 +1,81 @@
+package verifycode
+
+import (
+	"go-api-demo/pkg/app"
+	"go-api-demo/pkg/config"
+	"go-api-demo/pkg/helpers"
+	"go-api-demo/pkg/logger"
+	"go-api-demo/pkg/redis"
+	"go-api-demo/pkg/sms"
+	"strings"
+	"sync"
+)
+
+type VerifyCode struct {
+	Store Store
+}
+
+var once sync.Once
+var internalVerifyCode *VerifyCode
+
+// NewVerifyCode 单例模式获取
+func NewVerifyCode() *VerifyCode {
+	once.Do(func() {
+		internalVerifyCode = &VerifyCode{
+			Store: &RedisStore{
+				RedisClient: redis.Redis,
+				KeyPrefix:   config.GetString("app.name") + ":verifycode:",
+			},
+		}
+	})
+
+	return internalVerifyCode
+}
+
+// SendSMS 发送短信验证码，调用示例：verifycode.NewVerifyCode().SendSMS(request.Phone)
+func (vc *VerifyCode) SendSMS(phone string) bool {
+	//1. 生成短信验证码
+	code := vc.generateVerifyCode(phone)
+
+	//2. 方便本地和api自动测试
+	if !app.IsProduction() && strings.HasPrefix(phone, config.GetString("verifycode.debug_phone_prefix")) {
+		return true
+	}
+
+	//3. 发送短信
+	return sms.NewSMS().Send(phone, sms.Message{
+		Template: config.GetString("sms.aliyun.template_code"),
+		Data:     map[string]string{"code": code},
+	})
+}
+
+// CheckAnswer 检查用户提交的验证码是否正确，key可以是手机号或email
+func (vc *VerifyCode) CheckAnswer(key, answer string) bool {
+	logger.DebugJSON("验证码", "检查验证码", map[string]string{key: answer})
+
+	// 方便开发，在非生产环境下，具备特殊前缀的手机号和 Email后缀，会直接验证成功
+	if !app.IsProduction() &&
+		(strings.HasSuffix(key, config.GetString("verifycode.debug_phone_prefix")) ||
+			strings.HasSuffix(key, config.GetString("verifycode.debug_email_suffix"))) {
+		return true
+	}
+
+	return vc.Store.Verify(key, answer, false)
+}
+
+// generateVerifyCode 生成验证码，并放置于Redis中
+func (vc *VerifyCode) generateVerifyCode(key string) string {
+	//1. 生成随机验证码
+	code := helpers.RandomNumber(config.GetInt("verifycode.code_length"))
+
+	//2. 为方便开发，本地环境使用固定验证码
+	if app.IsLocal() {
+		code = config.GetString("verifycode.debug_code")
+	}
+
+	logger.DebugJSON("验证码", "生成验证码", map[string]string{key: code})
+
+	//3. 将验证码和key(邮箱或手机号) 存放到Redis中并设置过期时间
+	vc.Store.Set(key, code)
+	return code
+}
